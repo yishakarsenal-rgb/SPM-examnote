ef95ad999c13fe931ee6bce354e7585fbb1f61b3
yishakarsenal-rgb <yisahkarsenal@gmail.com>
2026-07-27 20:50:13 +0300

    update

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -54,8 +54,8 @@ const chapters = [
   {
     id: 7,
     title: "Risk Management",
-    color: "text-white-400",
-    bg: "bg-red-500/10 border-red-500/20",
+    color: "text-white-300",
+    bg: "bg-red-400/10 border-red-400/20",
     questions: 10,
   },
   {
